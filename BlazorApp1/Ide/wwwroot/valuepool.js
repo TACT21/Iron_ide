b776dc023e5a9a0ecb6b89f00016c63eccbeed10
TACT21 <53495996+TACT21@users.noreply.github.com>
2021-12-10 17:41:04 +0900

    5th

--- BlazorApp1/Ide/wwwroot/valuepool.js
+++ BlazorApp1/Ide/wwwroot/valuepool.js
@@ -1,8 +1,1 @@
-﻿var content_seth = "";
-function set_content_seth(s) {
-    console.log("set_content_seth called")
-    content_seth = String(s)
-}
-function get_content_seth() {
-    return content_seth
-}
+﻿
