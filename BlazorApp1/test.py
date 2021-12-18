6448cbf55d78b09d0f5eb9385476171992da9be9
TACT21 <53495996+TACT21@users.noreply.github.com>
2021-12-18 17:30:03 +0900

    cpp1st

--- BlazorApp1/test.py
+++
@@ -1,4 +0,0 @@
-def main():
-a = input()
-print(a)
-input()
--- BlazorApp1/test_2.py
+++
@@ -1,9 +0,0 @@
-# coding: UTF-8
-# メッセージの出力を行う
-hello = "Hello IronPython!"
-print(hello)
-hello = input()
-print(hello)
-
-# 表示を確認するまでの待機のために
-raw_input("\n［Enter］キーで終了します ．．．")
