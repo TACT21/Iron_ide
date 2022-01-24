f3d63a221579542c475c8c666d322828ff6cdf2e
TACT21 <takumokumoku@gmail.com>
2022-01-24 10:33:09 +0900

    Chenge to .net 5

---
+++ BlazorMonaco-master/SampleApp/wwwroot/esmjs.js
@@ -0,0 +1,3 @@
+﻿import 'monaco-editor/esm/vs/editor/browser/controller/coreCommands.js';
+import 'monaco-editor/esm/vs/editor/contrib/find/findController.js';
+import * as monaco from 'esm/editor.api.js';
