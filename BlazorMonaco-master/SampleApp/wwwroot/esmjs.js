8d992d8ca6996aa1099ebbf935f7bd650e13a670
TACT21 <53495996+TACT21@users.noreply.github.com>
2022-02-06 17:19:20 +0900

    Organize

--- BlazorMonaco-master/SampleApp/wwwroot/esmjs.js
+++
@@ -1,3 +0,0 @@
-﻿import 'monaco-editor/esm/vs/editor/browser/controller/coreCommands.js';
-import 'monaco-editor/esm/vs/editor/contrib/find/findController.js';
-import * as monaco from 'esm/editor.api.js';
