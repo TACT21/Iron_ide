48b228bffe91481a7c2cb6bb66c19a45bd298a63
TACT21 <53495996+TACT21@users.noreply.github.com>
2023-08-18 09:00:32 +0900

    Edit test modules

--- TestModule/test.py
+++ TestModule/test.py
@@ -1,4 +1,11 @@
-print("a")
-a = input()
-print(a)
+i = input()
+T = [(int(data)) for data in i.split(",")]
+
+
+for i in range(len(T)-1):
+    for j in range(0, len(T)-1-i):
+        if T[j] > T[j+1]:
+            T[j], T[j+1] = T[j+1], T[j]
+    print(T)
+
 input()
